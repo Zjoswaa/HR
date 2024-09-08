84e3b2e82019cbff4e52b1d110bccef525153cab
Zjoswaa <joshuakijkuit@hotmail.com>
2024-09-08 17:34:54 +0200

    Added up to W01.2.2O10

---
+++ Y1/OODP/W01.2.2O08/Program.cs
@@ -0,0 +1,22 @@
+﻿List<double> grades = new() { 6.5, 9.5, 4, 5, 4.5, 10, 7.1 };
+
+for (int i = 0; i < grades.Count; i++) {
+    Console.WriteLine($"Grade: {grades[i]}");
+    if (grades[i] < 5.5) {
+        while (true) {
+            Console.WriteLine("Retake this course? y/n");
+            string input = Console.ReadLine().ToUpper();
+            if (input == "Y") {
+                grades[i] += 1.0;
+                break;
+            } else if (input == "N") {
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Final grades:");
+for (int i = 0; i < grades.Count; i++) {
+    Console.WriteLine(grades[i]);
+}
---
+++ Y1/OODP/W01.2.2O10/Program.cs
@@ -0,0 +1,40 @@
+﻿Random rand = new(0);
+int min = 0;
+int max = 0;
+int answer = 0;
+bool guessed = false;
+
+Console.WriteLine("Time to play Guess The Number!");
+do {
+    Console.WriteLine("Give the minimum number: ");
+    min = int.Parse(Console.ReadLine());
+    Console.WriteLine("Give the maximum number: ");
+    max = int.Parse(Console.ReadLine());
+    if (min == max) {
+        Console.WriteLine("The minimum and maximum are equal. Incrementing the maximum by 1.");
+        max++;
+    } else if (min > max) {
+        Console.WriteLine("The minimum is higher than the maximum. Switching values.");
+        (min, max) = (max, min); // Swap variables
+    }
+
+    answer = rand.Next(min, max + 1);
+    guessed = false;
+
+    while (!guessed) {
+        Console.WriteLine($"Guess the number [{min}-{max}]");
+        int input = int.Parse(Console.ReadLine());
+        if (input == answer) {
+            Console.WriteLine($"{input} is correct!");
+            guessed = true;
+        } else if (answer < input) {
+            Console.WriteLine("Lower!");
+        } else {
+            Console.WriteLine("Higher!");
+        }
+    }
+
+    Console.WriteLine("Do you wish to play another round? Y to continue");
+} while (Console.ReadLine().ToUpper() == "Y");
+
+Console.WriteLine("Thank you for playing!");
