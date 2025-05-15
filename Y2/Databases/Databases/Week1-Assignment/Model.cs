60df98a34f26ebd365abcba03d5f4aa0be177711
Zjoswaa <joshuavdjagt@gmail.com>
2025-05-15 22:03:14 +0200

    Change database name

--- Y2/Databases/Databases/Week1-Assignment/Model.cs
+++ Y2/Databases/Databases/Week1-Assignment/Model.cs
@@ -4,7 +4,7 @@ namespace Model {
     public class TrialContext : DbContext {
         protected override void OnConfiguring(DbContextOptionsBuilder builder) {
             string UserID = "postgres";
-            string DBName = "postgres"; //change it accordingly
+            string DBName = "Week1-DB"; //change it accordingly
             string Host = "localhost"; //127.0.0.1
             string Port = "5432";
             builder.UseNpgsql($"User ID={UserID};Host={Host};Port={Port};Database={DBName};Pooling=true;");
